6e12115720bae49cf0d396b5dbc566ec58150da5
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-07-28 11:40:34 +0700

    Did some styling

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 const url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json';
@@ -9,45 +10,147 @@ req.send();
 req.onload = function(){
     const json = JSON.parse(req.responseText);
     const dataset = json.data;
-    //Dataset format: [ [year,data], [year,data], ...]
     
-//Declare the chart
-const width = 700;
-const height = 450;
-const marginTop = 20;
-const marginRight = 20;
-const marginBottom = 30;
-const marginLeft = 40;
+    const years = dataset.map(item => {
+        let quarter;
+        const temp = item[0].substring(5,7);
 
-//Declare the x (horizontal position) scale
-const x = d3.scaleBand()
-            .domain(dataset.map(d => d[0]))
-            .range([marginLeft, width - marginRight])
-            .padding(0.1);
+        if (temp === '01') {
+            quarter = 'Q1';
+        } else if (temp === '04') {
+            quarter = 'Q2';
+        } else if (temp === '07') {
+            quarter = 'Q3';
+        } else if (temp === '10') {
+            quarter = 'Q4';
+        }
+        return item[0].substring(0,4) + ' ' + quarter;
+    })
 
-//Declare the y (vertical position) scale
-const y = d3.scaleLinear()
-            .domain([0,d3.max(dataset.map(d => d[1]))])
-            .range([height - marginBottom, marginTop])
+    const yearsDate = dataset.map(item => new Date(item[0]));
 
-//Create SVG container
-const svg = d3.create("svg")
-            .attr('width',width)
-            .attr('height',height)
-            .attr('viewBox',[0, 0, width, height]) //Make chart svg responsive
-            .attr('style','max-width: 100%; height: auto;');
+    //Declare chart properties
+    const width = 700;
+    const height = 500;
+    const marginTop = 20;
+    const marginRight = 20;
+    const marginBottom = 30;
+    const marginLeft = 40;
+    const barWidth = width/275; //dataset.length = 275
+    const tooltip = d3.select('#container')
+                      .append('div')
+                      .attr('id','tooltip')
+                      .style('opacity',0);
+    const overlay = d3.select('#container')
+                      .append('div')
+                      .attr('class','overlay')
+                      .style('opacity',0);
 
-//Add a rect for each bar
-svg.append("g")
-    .attr('fill','steelblue')
-    .selectAll()
-    .data(dataset)
-    .join('rect')
-        .attr('x', (d) => x(d[0]))
-        .attr('y', (d) => y(d[1]))
-        .attr('width', x.bandwidth())
-        .attr('height', (d) => y(0) - y(d[1]));
+    //Create SVG container
+    const svg = d3.select('#container')
+    .append("svg")
+    .attr('width',width)
+    .attr('height',height)
+    .attr('viewBox',[0, 0, width-marginLeft, height+marginBottom]) //Make chart svg responsive
+    .attr('style','max-width: 100%; height: auto;');
+
+    const xMax = new Date(d3.max(yearsDate));
+    xMax.setMonth(xMax.getMonth()+3);
+    const xScale = d3.scaleTime()
+                     .domain([d3.min(yearsDate),xMax])
+                     .range([0,width])
+
+    const xAxis = d3.axisBottom().scale(xScale);
+
+    //Add x axis
+    svg.append('g')
+       .call(xAxis)
+       .attr('id','x-axis')
+       .attr('transform',`translate(0,${height-marginBottom})`);
+       
+    //Add legend to x-axis
+    svg.append('text')
+       .attr('x', width/2 - 20)
+       .attr('y', height + 10)
+       .text('More Information: http://www.bea.gov/national/pdf/nipaguid.pdf') 
+       .attr('class','info');
+
+    const GDP = dataset.map(item => item[1]);
     
-document.getElementById('container').append(svg.node());
+    let scaleGDP = [];
+    
+    const gdpMax = d3.max(GDP);
+    
+    const linearScale = d3.scaleLinear()
+                          .domain([0,gdpMax])
+                          .range([0,height - marginTop - marginBottom]);
+    
+    scaleGDP = GDP.map(item => linearScale(item));
+    
+    const yAxisScale = d3.scaleLinear()
+                         .domain([0,gdpMax])
+                         .range([height-marginBottom, marginTop]);
+    
+    const yAxis = d3.axisLeft(yAxisScale);
+    
+    //Add y axis
+    svg.append('g')
+       .call(yAxis)
+       .attr('id','y-axis')       
+       .attr('transform',`translate(0,0)`);       
+    
+    //Add legend to y-axis
+    svg.append('text')
+       .attr('x', -width/4 + 10)
+       .attr('y', 20)
+       .attr('transform','rotate(-90)')
+       .text('Gross Domestic Product');
+    
+    //Draw rects for each data
+    d3.select('svg')
+      .selectAll('rect')
+      .data(scaleGDP)
+      .enter()
+      .append('rect')
+      .attr('data-date', (d,i) => dataset[i][0])
+      .attr('data-gdp', (d,i) => dataset[i][1])
+      .attr('class','bar')
+      .attr('x', (d,i) => xScale(yearsDate[i]))
+      .attr('y', d => height - d - marginBottom - 1)
+      .attr('width', barWidth)
+      .attr('height', d => d)
+      .attr('index', (d,i) => i)
+      .attr('fill','steelblue')
+      .on('mouseover', (event,d) => {
+        //d is the height of the current rect
+        const i = event.currentTarget.getAttribute('index');
+        
+        // overlay
+        //     .transition()
+        //     .duration(0)
+        //     .style('height', d + 'px')
+        //     .style('width', barWidth + 'px')
+        //     .style('opacity',0.9)
+        //     .style('left',i * barWidth + 0 + 'px')
+        //     .style('top', height - d + 136 + 'px')
+        //     .style('transform','translateX(60px)');
+        tooltip.transition().duration(200).style('opacity',0.9);
+        tooltip
+            .html(
+                years[i] +
+                    '<br>' +
+                    '$' +
+                    GDP[i].toFixed(1).replace(/(\d)(?=(\d{3})+\.)/g,'$1,') +
+                    ' Billion'
+            )
+            .attr('data-date',dataset[i][0])
+            .style('left', i * barWidth + 30 + 'px')
+            .style('top', height+'px')
+            .style('transform', 'translateX(60px)') 
+      })
+      .on('mouseout', function(){
+        tooltip.transition().duration(200).style('opacity',0);
+        overlay.transition().duration(200).style('opacity',0);
+      });
 }
 
